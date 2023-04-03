b7d411a2a896d54caff7ec711238ba5fb979e935
baboshi <drhameid@yahoo.com>
2023-04-03 02:32:41 +0200

    Archive features added

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+
   //Used to compose an email
   document.querySelector("#compose-form").onsubmit=compose;
 
@@ -24,13 +25,15 @@ function compose_email() {
   document.querySelector('#compose-body').value = '';
 }
 
+
+
 function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
   document.querySelector("#email-content").style.display='none';
-
+  
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
   //showing the email
@@ -48,6 +51,7 @@ function load_mailbox(mailbox) {
       //entering the html
       element.innerHTML=`Sender: ${email.sender} Subject: ${email.subject} timeStamp: ${email.timestamp} ` ;
       document.querySelector("#emails-view").append(element);
+      //Adding the click to show function to the Email
       element.addEventListener("click",()=>{
         console.log(email);
         email_content(email.id);
@@ -55,15 +59,18 @@ function load_mailbox(mailbox) {
           method: 'PUT',
           body: JSON.stringify({
               read: true
+
           })
         })
         
-        
-    }
-      )
+      })
     });
   });
+
 }
+
+
+
 
 //function for submitting the mail
 function compose(){
@@ -81,6 +88,10 @@ function compose(){
   return false;
 }
 
+
+
+
+
 function email_content(x){
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'none';
@@ -92,7 +103,45 @@ function email_content(x){
     let subject=data.subject;
     let timeStamp=data.timestamp;
     let body=data.body;
-    document.querySelector("#email-content").innerHTML=`<h1>From: ${sender} to ${recipient}</h1><br><h2>Subject :${subject} Timestamp : ${timeStamp}</h2><br>${body}`
-   })
+    document.querySelector("#email-content").innerHTML=`<h1>From: ${sender} to ${recipient}</h1><br><h2>Subject :${subject} Timestamp : ${timeStamp}</h2><br>${body}<br>`
+    //adding archive button
+    console.log(data.archived)
+    let button=document.createElement("button");
+    
+    document.querySelector("#email-content").append(button);
+    if(data.archived){
+      button.innerHTML="Unarchive";
+    }
+    else{
+      button.innerHTML="Archive";
+    }
+    button.onclick=()=>{
+    if(data.archived==false){
+      fetch(`/emails/${x}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            archived: true
+        })
+      })
+      button.innerHTML="Unarchive";
+      load_mailbox('inbox');
+    
  
+    }
+    else{
+      
+        fetch(`/emails/${x}`,{
+          method:'PUT',
+          body:JSON.stringify({
+            archived:false
+          })
+        })
+        button.innerHTML="archive";
+        load_mailbox('inbox');
+      
+    }
+    
+  }
+  })
+
 }
